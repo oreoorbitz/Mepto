4fdae7dced2a9cd42a5ded34164751a6dfcd8627
oreoorbitz <oreoorbitz@gmail.com>
2026-05-01 14:36:45 -0600

    Auto format

--- src/event.ts
+++ src/event.ts
@@ -5,7 +5,6 @@
 import { type MeptoStatic } from './types'
 
 declare const mepto: MeptoStatic
-
 ;(function ($: MeptoStatic) {
   let _zid = 1
   const slice = Array.prototype.slice
@@ -32,7 +31,11 @@ declare const mepto: MeptoStatic
   const focus: Record<string, string> = { focus: 'focusin', blur: 'focusout' }
   const hover: Record<string, string> = { mouseenter: 'mouseover', mouseleave: 'mouseout' }
 
-  specialEvents.click = specialEvents.mousedown = specialEvents.mouseup = specialEvents.mousemove = 'MouseEvents'
+  specialEvents.click =
+    specialEvents.mousedown =
+    specialEvents.mouseup =
+    specialEvents.mousemove =
+      'MouseEvents'
 
   function zid(element: ZidTarget): number {
     return element._zid || (element._zid = _zid++)
@@ -67,10 +70,7 @@ declare const mepto: MeptoStatic
   }
 
   function eventCapture(handler: Handler, captureSetting?: boolean): boolean {
-    return (
-      (!!handler.del && !focusinSupported && handler.e in focus) ||
-      !!captureSetting
-    )
+    return (!!handler.del && !focusinSupported && handler.e in focus) || !!captureSetting
   }
 
   function realEvent(type: string): string {
@@ -105,24 +105,23 @@ declare const mepto: MeptoStatic
       if (handler.e in hover)
         callback = function (e: Event) {
           const related = (e as MouseEvent).relatedTarget
-          if (
-            !related ||
-            (related !== this && !$.contains(this as Element, related as Element))
-          )
+          if (!related || (related !== this && !$.contains(this as Element, related as Element)))
             return handler.fn.apply(this, arguments)
         }
       handler.del = delegator
       const cb = delegator || callback
       handler.proxy = function (e: Event) {
         e = compatible(e)
-        if ((e as Event & { isImmediatePropagationStopped(): boolean }).isImmediatePropagationStopped()) return
+        if (
+          (
+            e as Event & { isImmediatePropagationStopped(): boolean }
+          ).isImmediatePropagationStopped()
+        )
+          return
         ;(e as Event & { data?: unknown }).data = data
         const args = (e as Event & { _args?: unknown[] })._args
-      const result = cb.apply(
-          element,
-          args == undefined ? [e] : [e].concat(args as Event[])
-        )
-        if (result === false) e.preventDefault(), e.stopPropagation()
+        const result = cb.apply(element, args == undefined ? [e] : [e].concat(args as Event[]))
+        if (result === false) (e.preventDefault(), e.stopPropagation())
         return result
       }
       set.push(handler)
@@ -171,10 +170,7 @@ declare const mepto: MeptoStatic
     const args = 2 in arguments ? presetArgs : undefined
     if (isFunction(fn)) {
       const proxyFn = function () {
-        return fn.apply(
-          context,
-          args ? args.concat(slice.call(arguments)) : arguments
-        )
+        return fn.apply(context, args ? args.concat(slice.call(arguments)) : arguments)
       }
       ;(proxyFn as unknown as ZidTarget)._zid = zid(fn as unknown as ZidTarget)
       return proxyFn
@@ -195,16 +191,19 @@ declare const mepto: MeptoStatic
       throw new TypeError('expected function')
     }
   }
-
   ;($.fn as unknown as Record<string, any>).bind = function (event: any, data: any, callback: any) {
     return this.on(event, data, callback)
   }
-
   ;($.fn as unknown as Record<string, any>).unbind = function (event: any, callback: any) {
     return this.off(event, callback)
   }
-
-  ;($.fn as unknown as Record<string, any>).one = function (event: any, selector: any, data: any, callback: any, one: any) {
+  ;($.fn as unknown as Record<string, any>).one = function (
+    event: any,
+    selector: any,
+    data: any,
+    callback: any,
+    one: any
+  ) {
     return this.on(event, selector, data, callback, 1)
   }
 
@@ -214,8 +213,7 @@ declare const mepto: MeptoStatic
   const returnFalse = function () {
     return false
   }
-  const ignoreProperties =
-    /^([A-Z]|returnValue$|layer[XY]$|webkitMovement[XY]$)/
+  const ignoreProperties = /^([A-Z]|returnValue$|layer[XY]$|webkitMovement[XY]$)/
   const eventMethods: Record<string, string> = {
     preventDefault: 'isDefaultPrevented',
     stopImmediatePropagation: 'isImmediatePropagationStopped',
@@ -224,10 +222,7 @@ declare const mepto: MeptoStatic
 
   function compatible(event: Event, source?: Event): Event {
     const evt = event as Event & Record<string, unknown>
-    if (
-      source ||
-      !(evt as Event & { isDefaultPrevented?: () => boolean }).isDefaultPrevented
-    ) {
+    if (source || !(evt as Event & { isDefaultPrevented?: () => boolean }).isDefaultPrevented) {
       source || (source = event)
 
       $.each(eventMethods, function (name, predicate) {
@@ -250,12 +245,10 @@ declare const mepto: MeptoStatic
           ? (source as Event).defaultPrevented
           : 'returnValue' in source
             ? (source as Event & { returnValue: boolean }).returnValue === false
-            : (source as Event & { getPreventDefault?: () => boolean })
-                  .getPreventDefault &&
+            : (source as Event & { getPreventDefault?: () => boolean }).getPreventDefault &&
               (source as Event & { getPreventDefault?: () => boolean }).getPreventDefault!()
       )
-        (evt as Event & { isDefaultPrevented?: () => boolean }).isDefaultPrevented =
-          returnTrue
+        (evt as Event & { isDefaultPrevented?: () => boolean }).isDefaultPrevented = returnTrue
     }
     return event
   }
@@ -275,34 +268,43 @@ declare const mepto: MeptoStatic
     return compatible(proxy as unknown as Event, event)
   }
 
-  ;($.fn as unknown as Record<string, any>).delegate = function (selector: any, event: any, callback: any) {
+  ;($.fn as unknown as Record<string, any>).delegate = function (
+    selector: any,
+    event: any,
+    callback: any
+  ) {
     return this.on(event, selector, callback)
   }
-
-  ;($.fn as unknown as Record<string, any>).undelegate = function (selector: any, event: any, callback: any) {
+  ;($.fn as unknown as Record<string, any>).undelegate = function (
+    selector: any,
+    event: any,
+    callback: any
+  ) {
     return this.off(event, selector, callback)
   }
-
-  ;($.fn as unknown as Record<string, any>).on = function (event: any, selector: any, data: any, callback: any, one: any) {
+  ;($.fn as unknown as Record<string, any>).on = function (
+    event: any,
+    selector: any,
+    data: any,
+    callback: any,
+    one: any
+  ) {
     let autoRemove, delegator
     const $this = this
     if (event && !isString(event)) {
-      $.each(
-        event as Record<string, (...args: unknown[]) => unknown>,
-        function (type, fn) {
-          $this.on(type, selector, data, fn, one)
-        }
-      )
+      $.each(event as Record<string, (...args: unknown[]) => unknown>, function (type, fn) {
+        $this.on(type, selector, data, fn, one)
+      })
       return $this
     }
 
     if (!isString(selector) && !isFunction(callback) && callback !== false)
-      (callback = data as (...args: unknown[]) => unknown),
+      ((callback = data as (...args: unknown[]) => unknown),
         (data = selector),
-        (selector = undefined)
+        (selector = undefined))
 
     if (callback === undefined || data === false)
-      (callback = data as (...args: unknown[]) => unknown), (data = undefined)
+      ((callback = data as (...args: unknown[]) => unknown), (data = undefined))
 
     if (callback === false) callback = returnFalse as (...args: unknown[]) => unknown
 
@@ -310,10 +312,7 @@ declare const mepto: MeptoStatic
       if (one)
         autoRemove = function (e: Event) {
           remove(element, e.type, callback)
-          return (callback as (...args: unknown[]) => unknown).apply(
-            this,
-            arguments
-          )
+          return (callback as (...args: unknown[]) => unknown).apply(this, arguments)
         }
 
       if (selector)
@@ -327,10 +326,7 @@ declare const mepto: MeptoStatic
               currentTarget: match,
               liveFired: element,
             })
-            return (autoRemove || callback).apply(
-              match,
-              [evt].concat(slice.call(arguments, 1))
-            )
+            return (autoRemove || callback).apply(match, [evt].concat(slice.call(arguments, 1)))
           }
         }
 
@@ -344,22 +340,21 @@ declare const mepto: MeptoStatic
       )
     })
   }
-
-  ;($.fn as unknown as Record<string, any>).off = function (event: any, selector: any, callback: any) {
+  ;($.fn as unknown as Record<string, any>).off = function (
+    event: any,
+    selector: any,
+    callback: any
+  ) {
     const $this = this
     if (event && !isString(event)) {
-      $.each(
-        event as Record<string, (...args: unknown[]) => unknown>,
-        function (type, fn) {
-          $this.off(type, selector, fn)
-        }
-      )
+      $.each(event as Record<string, (...args: unknown[]) => unknown>, function (type, fn) {
+        $this.off(type, selector, fn)
+      })
       return $this
     }
 
     if (!isString(selector) && !isFunction(callback) && callback !== false)
-      (callback = selector as (...args: unknown[]) => unknown),
-        (selector = undefined)
+      ((callback = selector as (...args: unknown[]) => unknown), (selector = undefined))
 
     if (callback === false) callback = returnFalse as (...args: unknown[]) => unknown
 
@@ -372,7 +367,6 @@ declare const mepto: MeptoStatic
       )
     })
   }
-
   ;($.fn as unknown as Record<string, any>).trigger = function (event: any, args: any) {
     const evt =
       isString(event) || $.isPlainObject(event)
@@ -383,13 +377,11 @@ declare const mepto: MeptoStatic
       // handle focus(), blur() by calling them directly
       if (
         (evt as Event).type in focus &&
-        typeof (this as unknown as Record<string, unknown>)[(evt as Event).type] ==
-          'function'
+        typeof (this as unknown as Record<string, unknown>)[(evt as Event).type] == 'function'
       )
         (this as unknown as Record<string, () => void>)[(evt as Event).type]()
       // items in the collection might not be DOM elements
-      else if ('dispatchEvent' in this)
-        (this as EventTarget).dispatchEvent(evt as Event)
+      else if ('dispatchEvent' in this) (this as EventTarget).dispatchEvent(evt as Event)
       else $(this).triggerHandler(evt as Event, args)
     })
   }
@@ -407,7 +399,9 @@ declare const mepto: MeptoStatic
         function (_: number, handler: Handler) {
           result = handler.proxy(e)
           if (
-            (e as Event & { isImmediatePropagationStopped(): boolean }).isImmediatePropagationStopped()
+            (
+              e as Event & { isImmediatePropagationStopped(): boolean }
+            ).isImmediatePropagationStopped()
           )
             return false
         }
@@ -424,21 +418,18 @@ declare const mepto: MeptoStatic
   )
     .split(' ')
     .forEach(function (event: string) {
-      const $fn = ($.fn as unknown) as Record<string, (callback?: (...args: unknown[]) => unknown) => unknown>
+      const $fn = $.fn as unknown as Record<
+        string,
+        (callback?: (...args: unknown[]) => unknown) => unknown
+      >
       $fn[event] = function (callback) {
-        return 0 in arguments
-          ? this.bind(event, callback)
-          : this.trigger(event)
+        return 0 in arguments ? this.bind(event, callback) : this.trigger(event)
       }
     })
-
   ;($.Event as unknown as (type: any, props?: any) => Event) = function (type: any, props?: any) {
     if (!isString(type))
-      (props = type as Record<string, unknown>),
-        (type = (props as Record<string, string>).type)
-    const event = document.createEvent(
-      specialEvents[type as string] || 'Events'
-    )
+      ((props = type as Record<string, unknown>), (type = (props as Record<string, string>).type))
+    const event = document.createEvent(specialEvents[type as string] || 'Events')
     let bubbles = true
     if (props)
       for (const name in props)
